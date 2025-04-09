41b936261e8a5e912d7c6cbc1ce39ce6693c9195
Sk Nasrul <sk.nasrul.islam.06@gmail.com>
2025-04-09 17:59:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sudo')
+def sudo():
+    return render_template('sudo.html')
