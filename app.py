2051916a77913e90349ced9c4296c4a693fc7c48
Sk Nasrul <sk.nasrul.islam.06@gmail.com>
2025-05-03 19:08:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('yt.html')
 
 @app.route('/download', methods=['POST'])
 def download():
