d7dafbf17e4ed930c0c15f1743f8bf918e755c06
Sk Nasrul <sk.nasrul.islam.06@gmail.com>
2025-05-03 19:06:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
+from pytube import YouTube
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form['url']
+    yt = YouTube(url)
+    stream = yt.streams.get_highest_resolution()
+    filepath = stream.download()
+    return send_file(filepath, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
